dee784b4ed3e0b33a59d50909acc4053dba14fe0
Johan Eriksson <johan.eriksson@avaloninnovation.com>
2023-10-19 13:32:21 +0200

    Translate from swedish.

--- go_to_the_bar.py
+++ go_to_the_bar.py
@@ -2,56 +2,55 @@ import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# Öppna Excel-filen
+# Open the Excel file
 excel_file = openpyxl.load_workbook('C:/Users/avalonuser/Desktop/dummy_stapel.xlsx')
 sheet = excel_file.active
 
-# Skapa tomma listor för data
-namn = []
-min_varde = []
-medelvarde = []
-max_varde = []
+# Create empty lists for data
+names = []
+min_value = []
+mean_value = []
+max_value = []
 
-# Loopa igenom rader i Excel-arket och hämta data
-for row in sheet.iter_rows(min_row=2, values_only=True):  # Börja från rad 2 för att undvika rubrikerna
-    namn.append(row[0])
-    min_varde.append(row[1])
-    medelvarde.append(row[2])
-    max_varde.append(row[3])
+# Loop through rows in the Excel sheet and retrieve data
+for row in sheet.iter_rows(min_row=2, values_only=True):  # Start from row 2 to skip the headers
+    names.append(row[0])
+    min_value.append(row[1])
+    mean_value.append(row[2])
+    max_value.append(row[3])
 
-# Antalet namn
-antal_namn = len(namn)
+# Number of names
+num_names = len(names)
 
-# Bredden på staplarna och mellanrummet mellan staplarna
-bredd = 0.2
-mellanrum = 0.2
+# Width of the bars and the gap between them
+width = 0.2
+spacing = 0.2
 
-# Skapa x-koordinater för staplarna med mellanrum
-x = np.arange(antal_namn)
+# Create x-coordinates for the bars with spacing
+x = np.arange(num_names)
 
-# Skapa ett diagram med tre staplar per namn och ange färger
-plt.bar(x - mellanrum, min_varde, width=bredd, label='Min', color='darkslategrey')
-plt.bar(x, medelvarde, width=bredd, label='Medel', color='goldenrod')
-plt.bar(x + mellanrum, max_varde, width=bredd, label='Max', color='firebrick')
+# Create a chart with three bars per name and specify colors
+plt.bar(x - spacing, min_value, width=width, label='Min', color='darkslategrey')
+plt.bar(x, mean_value, width=width, label='Mean', color='goldenrod')
+plt.bar(x + spacing, max_value, width=width, label='Max', color='firebrick')
 
-# Lägg till horisontell grid
+# Add horizontal gridlines
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
-# Justera x-axeln
-plt.xticks(x, namn)
+# Adjust the x-axis
+plt.xticks(x, names)
 
-# Lägg till etiketter och en legend
+# Add labels and a legend
 plt.xlabel('Runs')
 plt.ylabel('Temperature [°C]')
-plt.title("Titel?")
+plt.title("Title?")
 plt.legend()
 
-# Lägg till värdena ovanför staplarna med formatet .3g och samma färger som staplarna
-for i in range(antal_namn):
-    plt.text(x[i] - mellanrum, min_varde[i], f"{min_varde[i]:.3g}", ha='center', va='bottom', color='darkslategrey')
-    plt.text(x[i], medelvarde[i], f"{medelvarde[i]:.3g}", ha='center', va='bottom', color='goldenrod')
-    plt.text(x[i] + mellanrum, max_varde[i], f"{max_varde[i]:.3g}", ha='center', va='bottom', color='firebrick')
+# Add values above the bars in the format .3g and use the same colors as the bars
+for i in range(num_names):
+    plt.text(x[i] - spacing, min_value[i], f"{min_value[i]:.3g}", ha='center', va='bottom', color='darkslategrey')
+    plt.text(x[i], mean_value[i], f"{mean_value[i]:.3g}", ha='center', va='bottom', color='goldenrod')
+    plt.text(x[i] + spacing, max_value[i], f"{max_value[i]:.3g}", ha='center', va='bottom', color='firebrick')
 
-# Visa diagrammet
+# Show the chart
 plt.show()
