dd583d42e482333310cbac273555c00c889ad87d
Johan Eriksson <johan.eriksson@avaloninnovation.com>
2023-10-23 13:48:45 +0200

    Added possibility to subplot evenness and level in bar plot.

--- go_to_the_bar.py
+++ go_to_the_bar.py
@@ -21,7 +21,7 @@ def find_start_row(sheet):
             return row_num
     return None
 
-def create_bar_chart(file_path, figure_size=(10, 6), savefig=False, text_size=12, text_font='sans-serif'):
+def create_bar_chart(file_path, figure_size=(10, 6), savefig=False, text_size=12, text_font='sans-serif', create_score=False):
     """
     Create a bar chart from data in an Excel file.
 
@@ -31,6 +31,7 @@ def create_bar_chart(file_path, figure_size=(10, 6), savefig=False, text_size=12
     - savefig (bool): Whether to save the plot as an image file.
     - text_size (int): Optional. The font size for the text labels above the bars.
     - text_font (str): Optional. The font style for the text labels above the bars.
+    - create_score (bool): Whether to subplot a evenness and level score.
 
     This function reads data from the Excel file and creates a bar chart with three bars (Min, Mean, Max) per data point.
     The chart includes horizontal gridlines and labels. You can customize the figure size using the figure_size parameter.
@@ -38,63 +39,47 @@ def create_bar_chart(file_path, figure_size=(10, 6), savefig=False, text_size=12
     # Open the Excel file
     excel_file = openpyxl.load_workbook(file_path)
 
-    # Loop through all sheets in the Excel file
     for sheet in excel_file.sheetnames:
         current_sheet = excel_file[sheet]
-        #sheet = excel_file.active
-
-        # Find the starting row
         start_row = find_start_row(current_sheet)
         if start_row is None:
             print("No numeric data found in column 2.")
             return
 
-        # Create empty lists for data
         names = []
         min_value = []
         mean_value = []
         max_value = []
 
-        # Loop through rows in the Excel sheet starting from the identified row
-        for row in current_sheet.iter_rows(min_row=2, values_only=True):  # Start from row 2 to skip the headers
+        for row in current_sheet.iter_rows(min_row=2, values_only=True):
             names.append(row[0])
             min_value.append(row[1])
             mean_value.append(row[2])
             max_value.append(row[3])
 
-        # Number of names
         num_names = len(names)
-
-        # Width of the bars and the gap between them
         width = 0.2
         spacing = 0.2
-
-        # Create x-coordinates for the bars with spacing
         x = np.arange(num_names)
 
-        # Set the figure size
         plt.figure(figsize=figure_size)
 
-        # Create a chart with three bars per name and specify colors
+
+        if create_score:
+            ax1 = plt.subplot(211)  # Skapa den första subplotten (2 rader, 1 kolumn, första plats)
+        
         plt.bar(x - spacing, min_value, width=width, label='Min', color='darkslategrey')
         plt.bar(x, mean_value, width=width, label='Mean', color='goldenrod')
         plt.bar(x + spacing, max_value, width=width, label='Max', color='firebrick')
 
-        # Add horizontal gridlines
         plt.grid(axis='y', linestyle='--', alpha=0.7)
-
-        # Adjust the x-axis
         plt.xticks(x, names)
-
-        # Add labels and a legend
+        filename = file_path.split("/")[-1].split("\\")[-1]
         plt.ylabel('Temperature [°C]')
-        plt.title(sheet)
+        plt.title(f'{filename}: {sheet}')
         plt.legend()
-
-        # Value for mooving the text above the bars to the left. Can be needed if numbers colides with bars.
         delta = 0.00
 
-        # Add values above the bars with customizable font size and style
         for i in range(num_names):
             plt.text(x[i] - spacing - delta, min_value[i], f"{min_value[i]:.3g}", ha='center', va='bottom',
                     color='darkslategrey', fontsize=text_size, family=text_font)
@@ -103,12 +88,25 @@ def create_bar_chart(file_path, figure_size=(10, 6), savefig=False, text_size=12
             plt.text(x[i] + spacing - delta, max_value[i], f"{max_value[i]:.3g}", ha='center', va='bottom',
                     color='firebrick', fontsize=text_size, family=text_font)
 
+        if create_score:
+            # Lägg till en subplott för (max - min) / mean under stapeldiagrammet
+            ax2 = plt.subplot(212, sharex=ax1)  # Skapa den andra subplotten (2 rader, 1 kolumn, andra plats)
+            #ratio = [(max_val - min_val) / mean_val for max_val, min_val, mean_val in zip(max_value, min_value, mean_value)]
+            ratio = [(((max_val - min_val) + (max_val - mean_val) + (mean_val - min_val) )/ 3)/(mean_val) for max_val, min_val, mean_val in zip(max_value, min_value, mean_value)]
+            plt.bar(x, ratio, width=width, label='Evenness and level score', color='royalblue')
+            plt.ylabel('Evenness and level score')
+            plt.legend()
+            plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+            for i in range(num_names):
+                plt.text(x[i], ratio[i], f"{ratio[i]:.3g}",
+                        ha='center', va='bottom', color='royalblue', fontsize=text_size, family=text_font)
 
         if savefig:
-                path = file_path.split(".xlsx")[0]
-                plt.savefig(f'{path}_{sheet}.png', transparent=True)
+            path = file_path.split(".xlsx")[0]
+            plt.savefig(f'{path}_{sheet}.png', transparent=True)
+            print(f'Saved file: {path}_{sheet}.png')
 
-        # Show the chart
         plt.show()
 
 def create_radar_subplots(file_path):
@@ -198,10 +196,11 @@ def create_radar_subplots(file_path):
     plt.show()
 
 # Example usage with a custom figure size (e.g., 12x8 inches)
-create_bar_chart(r'C:\Users\avalonuser\Desktop\Ytter- och centrumtemp.xlsx',#'C:/Users/avalonuser/Desktop/dummy_stapel.xlsx', 
+create_bar_chart(r'C:\Users\avalonuser\Desktop\Ytter- och centrumtemp.xlsx',
                  figure_size = (16, 8),
                  savefig = True,
                  text_size=8, 
-                 text_font='serif')
+                 text_font='serif',
+                 create_score = False)
 
 # create_radar_subplots('C:/Users/avalonuser/Desktop/dummy_stapel.xlsx')
